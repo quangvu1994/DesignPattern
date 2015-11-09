ebcb849c36ab40805123afa2fe371f954c6360e7
Quang Vu <qdv2130@rit.edu>
2015-11-08 22:36:20 -0500

    Added Iterator interface

--- src/Iterator/SongIterator.java
+++ src/Iterator/SongIterator.java
@@ -3,6 +3,11 @@ import java.util.Iterator;
 /**
  * Created by Quang on 11/8/15.
  */
+
+/**
+ * @desc defines a creating iterator method that convert all type of collections
+ * into iterator
+ */
 public interface SongIterator {
     public Iterator createIterator();
 }
